1edf1e714102f120ab9420a7d7bb213a33c37abc
christian <christiandavidphoto@gmail.com>
2018-06-07 14:16:52 -0400

    getWeather func

--- src/App.js
+++ src/App.js
@@ -4,7 +4,19 @@ import Titles from './components/Titles';
 import Weather from './components/Weather';
 import './App.css';
 
+// my personal api key
+const API_KEY = '53156c8bfca9a7c97cbf1e0d210c8d3b';
+
 class App extends Component {
+  getWeather = async () => {
+    const api_call = await fetch(
+      `http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEY}&units=metric`
+    );
+
+    // gather the data from the call and store it as JSON
+    const data = await api_call.json();
+  };
+
   render() {
     return (
       <Fragment>
